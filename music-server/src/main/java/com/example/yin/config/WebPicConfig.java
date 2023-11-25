3ae0d74bfe9e1e39e4f0de28854aa92fc3121dcc
ghj1040110333 <1040110333@qq.com>
2023-11-25 19:02:57 +0800

    代码优化

--- music-server/src/main/java/com/example/yin/config/WebPicConfig.java
+++ music-server/src/main/java/com/example/yin/config/WebPicConfig.java
@@ -17,18 +17,12 @@ public class WebPicConfig implements WebMvcConfigurer {
     //TODO 这个配置类的目的是什么  就是注册了一个类似于拦截器吧  看到对应的资源 会将其修改成相应的地址
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/img/avatorImages/**")
-                .addResourceLocations(Constants.AVATOR_IMAGES_PATH);
-        registry.addResourceHandler("/img/singerPic/**")
-                .addResourceLocations(Constants.SINGER_PIC_PATH);
-        registry.addResourceHandler("/img/songPic/**")
-                .addResourceLocations(Constants.SONG_PIC_PATH);
-        registry.addResourceHandler("/song/**")
-                .addResourceLocations(Constants.SONG_PATH);
-        registry.addResourceHandler("/img/songListPic/**")
-                .addResourceLocations(Constants.SONGLIST_PIC_PATH);
-        registry.addResourceHandler("/img/swiper/**")
-                .addResourceLocations(Constants.BANNER_PIC_PATH);
+        registry.addResourceHandler("/img/avatorImages/**").addResourceLocations(Constants.AVATOR_IMAGES_PATH);
+        registry.addResourceHandler("/img/singerPic/**").addResourceLocations(Constants.SINGER_PIC_PATH);
+        registry.addResourceHandler("/img/songPic/**").addResourceLocations(Constants.SONG_PIC_PATH);
+        registry.addResourceHandler("/song/**").addResourceLocations(Constants.SONG_PATH);
+        registry.addResourceHandler("/img/songListPic/**").addResourceLocations(Constants.SONGLIST_PIC_PATH);
+        registry.addResourceHandler("/img/swiper/**").addResourceLocations(Constants.BANNER_PIC_PATH);
     }
 
 }
