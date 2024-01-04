daafa8736ebe5dbdf64b768f87923f58a62d18ac
劈开/穆欣 <1040110333@qq.com>
2024-01-04 15:16:01 +0800

    环境部署

--- music-client/vue.config.js
+++ music-client/vue.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
     proxy: {
       '/music': {
         // 以 “/api” 开头的 代理到 下边的 target 属性 的值 中
-        target: 'http://120.46.77.1:8080/',
+        target: 'http://120.46.77.1:1880/',
         changeOrigin: true, // 是否改变域名：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
         ws: true, // 是否启用websockets
         pathRewrite: {
@@ -20,7 +20,7 @@ module.exports = defineConfig({
   chainWebpack: config => {
     config.plugin('define').tap(definitions => {
       Object.assign(definitions[0]['process.env'], {
-        NODE_HOST: '"http://120.46.77.1:8080"',
+        NODE_HOST: '"http://120.46.77.1:1880"',
       });
       return definitions;
     });
